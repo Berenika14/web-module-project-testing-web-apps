58075a0b41dbf4deb774403bc1cc78f02e562639
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2022-01-13 18:17:55 -0500

    added a beforeEach to render the contact from before each test happens , wrote 7 tests

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,41 +1,89 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import {render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import ContactForm from './ContactForm';
+import ContactForm from "./ContactForm";
 
-test('renders without errors', ()=>{
-    
+beforeEach(() => {
+  render(<ContactForm />);
 });
 
-test('renders the contact form header', ()=> {
-    
+// test("renders without errors", () => {
+//   render(<ContactForm />);
+// });
+
+test("renders the contact form header", () => {
+  // render(<ContactForm/>)
+  const header = screen.queryByText(/contact form/i);
+  expect(header).toBeInTheDocument();
 });
 
-test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  const firstNameEl = screen.getByTestId("firstName");
+  expect(screen.queryAllByTestId("error").length).toBe(0);
+
+  userEvent.type(firstNameEl, "Bere");
+  const errors = screen.queryAllByText(/must have at least 5 characters/i);
+  expect(errors.length).toBe(1);
 });
 
-test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  const firstNameEl = screen.getByPlaceholderText("Edd");
+  const lastNameEl = screen.getByPlaceholderText("Burke");
+  const emailEL = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+  expect(screen.queryAllByPlaceholderText("error").length).toBe(0);
+
+  userEvent.type(firstNameEl);
+  userEvent.type(lastNameEl);
+  userEvent.type(emailEL);
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const errors = screen.queryAllByTestId("error");
+  expect(errors.length).toBe(3);
 });
 
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {
+  const firstNameEl = screen.getByPlaceholderText("Edd");
+  const lastNameEl = screen.getByPlaceholderText("Burke");
+  const emailEL = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+  expect(screen.queryAllByPlaceholderText("error").length).toBe(0);
+
+  userEvent.type(firstNameEl, "Berenika");
+  userEvent.type(lastNameEl, "Ahmetaj");
+  userEvent.type(emailEL);
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const errors = screen.queryAllByText(/must be a valid email address/i);
+  expect(errors.length).toBe(1);
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+  const emailEL = screen.getByPlaceholderText("bluebill1049@hotmail.com");
+  expect(screen.queryAllByPlaceholderText("error").length).toBe(0);
+
+  userEvent.type(emailEL, "nika");
+  const errors = screen.queryAllByText(/must be a valid email address/i);
+  expect(errors.length).toBe(1);
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+  const lastNameEl = screen.queryByPlaceholderText("Burke");
+  expect(screen.queryAllByPlaceholderText("error").length).toBe(0);
+
+  userEvent.type(lastNameEl, "");
+
+  const button = screen.getByRole("button");
+  userEvent.click(button);
+
+  const errors = screen.queryAllByText(/lastName is a required field/i);
+  expect(errors.length).toBe(1);
 });
 
-test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
-});
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
 
-test('renders all fields text when all fields are submitted.', async () => {
-});
+test("renders all fields text when all fields are submitted.", async () => {});
